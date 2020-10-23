a8d2381826c3d03424f728a43f79d8ba12302fd0
Tejal <Tejal@Tejal-PC>
2020-10-23 20:16:53 +0530

    First Commit

---
+++ DemoForGit/src/main/java/com/spboot/DemoForGitApplication.java
@@ -0,0 +1,13 @@
+package com.spboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForGitApplication.class, args);
+	}
+
+}
---
+++ DemoForGit/src/test/java/com/spboot/DemoForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
